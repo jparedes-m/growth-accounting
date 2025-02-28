addfeac92d2c6e2fe81a2c87d66101c034ad0eff
jparedes-m <jparedes2407@gmail.com>
2025-02-28 16:41:54 +0100

    Graph as question d solution

--- code/code.r
+++ code/code.r
@@ -5,6 +5,9 @@
 # Data conformation ----
 source("code/data conformation.r")
 
+data <- data %>% 
+  filter(year %in% 2009:2019)
+
 # Question b: Output per worker ----
 data %>% 
   filter(sector == "Total Economy (TOT)" | sector == "Market Economy (MARKT)") %>% 
@@ -31,7 +34,10 @@ a = 1/3
 # Now we can calculate the TFP
 data <- data %>% 
   mutate(A_e = y_r/((K/Y_r)^(a/(1-a)) * h_e),
-         A_w = y_r/((K/Y_r)^(a/(1-a)) * h_w))
+         A_w = y_r/((K/Y_r)^(a/(1-a)) * h_w)) %>% 
+  group_by(sector) %>%
+  mutate(gamma_y = 100*(log(y_r) - lag(log(y_r)))) %>%
+  ungroup()
 
 # Note: (written in google docs)
 #For Capital: We are currently using the account called all the assets, this includes non-tangible assets (software, databases, R&D), it can be discussed that we can do the TFP decomposition only using tangible assets (fixed capital). 
@@ -44,10 +50,74 @@ data <- data %>%
   filter(sector %in% sectors)
 
 ggplot(data) +
- aes(x = year, y = A_e, colour = sector, size = y_r) +
+ aes(x = year, y = A_e, size = gamma_y) +
+ geom_line(colour = 'navyblue') +
+ scale_x_continuous(n.breaks = max(data$year) - min(data$year)+1) +
+ theme_light() +
+ labs(title = "TFP Residual", subtitle = "Computed with Share_E",
+      y = "TFP Residual", x = "Year", size = "Real Value Added Growth Rate (%):") +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 14, face = "bold", hjust = 0.5),
+        legend.position = 'bottom', legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10), 
+        legend.key = element_rect(fill = "white", colour = "white"), 
+        legend.box = "vertical",
+        axis.text.x = element_text(size = 10, angle = 90))+
+ facet_wrap(vars(sector))
+
+
+
+
+ggplot(data) +
+ aes(x = year, y = A_e) +
+ geom_line(colour = "navyblue") +
+ theme_light() +
+ labs(title = "TFP Residual", subtitle = "Computed with Share_E",
+      y = "TFP Residual", x = "Year") +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 14, face = "bold", hjust = 0.5),
+        legend.position = 'bottom', legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10), 
+        legend.key = element_rect(fill = "white", colour = "white"), 
+        legend.box = "vertical")+
+ facet_wrap(vars(sector))
+
+
+ggplot(data) +
+ aes(x = year, y = gamma_y) +
+ geom_line(colour = "navyblue") +
+ theme_light() +
+ labs(title = "Real Value Added Growth Rate", subtitle = "2020 = 100",
+      y = "(%)", x = "Year") +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 14, face = "bold", hjust = 0.5),
+        legend.position = 'bottom', legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10), 
+        legend.key = element_rect(fill = "white", colour = "white"), 
+        legend.box = "vertical")+
+ facet_wrap(vars(sector))
+
+
+
+
+
+
+ ggplot(data) +
+ aes(x = year, y = A_w, colour = sector, size = y_r) +
  geom_line() +
  scale_color_hue(direction = 1) +
- theme_minimal() +
+ theme_light() +
+ labs(title = "TFP Residual", subtitle = "Computed with Share_W",
+      y = "TFP Residual", x = "Year",
+      colour = "Sector:", size = "Real Value Added growth rate") +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 14, face = "bold", hjust = 0.5),
+        legend.position = 'bottom', legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10), 
+        legend.key = element_rect(fill = "white", colour = "white"), 
+        legend.box = "vertical")+
  facet_wrap(vars(sector))
+
+ # 
 
 View(data)
